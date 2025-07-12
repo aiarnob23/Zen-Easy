46d6f2777f3a3afec1d18c7ad0577c147c8ea306
aiarnob23 <aiarnob23@gmail.com>
2025-07-12 21:37:11 +0600

    backend server added

--- frontend/src/utils/baseUrl.ts
+++ frontend/src/utils/baseUrl.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const serverBaseUrl = axios.create({
     // baseURL:"http://localhost:4000/api/v1",
-    baseURL:"https://zen-easy-bd-backend.vercel.app",
+    baseURL:"https://zen-easy-bd-backend.vercel.app/api/v1",
     withCredentials:true,
 })
