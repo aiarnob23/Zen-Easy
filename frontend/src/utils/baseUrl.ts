acc669cc47f71c2349213e0e18e90f377f20b2cf
aiarnob23 <aiarnob23@gmail.com>
2026-02-15 16:53:00 +0600

    server base url udpated

--- frontend/src/utils/baseUrl.ts
+++ frontend/src/utils/baseUrl.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const serverBaseUrl = axios.create({
     //baseURL:"http://localhost:4000/api/v1",
     // baseURL:"https://zen-easy-bd-backend.vercel.app/api/v1",
-    baseURL:"https://zen-easy-bd-backend.vercel.app/api/v1",
+    baseURL:"https://zen-easy-4is1.vercel.app/api/v1",
     withCredentials:true,
 }) 
